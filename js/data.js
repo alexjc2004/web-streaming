d22c7228419ba2681990fe7f7a580376a027a52b
alexjc2004 <134894094+alexjc2004@users.noreply.github.com>
2026-06-25 11:57:54 -0400

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,26 @@
+// Lista para la primera categoría "Películas"
+const movies = [
+    "Naruto",
+    "The Avengers",
+    "Spider-Man",
+    "Avatar",
+    "Marty Supreme",
+    "Challengers",
+    "The Legend of Aang The Last Airbender",
+    "The Rip",
+    "Mario Galaxy"
+];
+
+// Lista para la segunda categoría "Nuevas Películas"
+const newMovies = [
+    "Deadpool 3",
+    "Dune: Part Two",
+    "Oppenheimer",
+    "Barbie",
+    "The Flash",
+    "Indiana Jones 5",
+    "Mission: Impossible 7",
+    "Wonka",
+    "Aquaman 2",
+    "John Wick 4"
+];
